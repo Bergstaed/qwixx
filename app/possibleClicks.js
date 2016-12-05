2ff689272884f71cfc04cfdb289cae060e6d0cdc
Bergstaed <diemailbittezumir@gmx.de>
2016-12-05 05:13:21 +0100

    .gitignore is now working

--- app/possibleClicks.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var PossibleClicks = (function () {
-    function PossibleClicks() {
-    }
-    return PossibleClicks;
-}());
-exports.PossibleClicks = PossibleClicks;
-//# sourceMappingURL=possibleClicks.js.map
